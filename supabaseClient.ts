6219fed70c6a6c3fc70d1763290333d916034900
aguswarno-droid <agus.warno@gmail.com>
2026-08-10 20:38:30 +0700

    feat: connect app to supabase database

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl =
+    process.env.NEXT_PUBLIC_SUPABASE_URL ||
+    "https://euvuegovsakdeeaibkne.supabase.co";
+
+const supabaseAnonKey =
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+    "sb_publishable_mP64vQohAsEKxZH01grh0g_oMb6Vfkb"; // masukkan Publishable Key Supabase kamu di sini
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
